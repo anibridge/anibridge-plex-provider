f66d139356cc0f6d63f19d7c91dc599dbdab3283
Elias Benbourenane <eliasbenbourenane@gmail.com>
2025-11-26 22:56:16 +0000

    chore: Repo setup and cleanup

--- src/anibridge_example_provider/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Example providers for the AniBridge project."""
-
-from anibridge_example_provider.example_library import ExampleLibraryProvider
-from anibridge_example_provider.example_list import ExampleListProvider
-
-__all__ = [
-    "ExampleLibraryProvider",
-    "ExampleListProvider",
-]
